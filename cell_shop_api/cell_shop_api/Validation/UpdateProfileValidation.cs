33004b7230c98fe20a883e27cd28078266211c22
Huynh Tan Dat <huynhdatIT.focus@gmail.com>
2022-07-17 11:57:03 +0700

    sf

--- cell_shop_api/cell_shop_api/Validation/UpdateProfileValidation.cs
+++ cell_shop_api/cell_shop_api/Validation/UpdateProfileValidation.cs
@@ -7,10 +7,6 @@ namespace cell_shop_api.Validation
     {
         public UpdateProfileValidation()
         {
-            RuleFor(x=>x.FullName).NotEmpty();
-            RuleFor(x => x.Email).NotEmpty().EmailAddress();
-            RuleFor(x => x.Birthday).Null();
-
         }
     }
 }
